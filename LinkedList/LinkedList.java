f3c502c89551185c02a56a9e39e4e3f66d52fa05
Manas Shukla <mshukla@apple.com>
2019-11-25 11:22:29 +0530

    Adding more functions to the Singly Linked List

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,7 +1,14 @@
+/**
+ * This class deals with the basic operations of Linked Lists viz insert, insert at first location, traverse
+ */
 public class LinkedList {
 
     public Node head;
 
+    /**
+     * insert the node value at the last of list
+     * @param node_val
+     */
     public void insert(int node_val){
 
         Node node = new Node(node_val);
@@ -16,21 +23,52 @@ public class LinkedList {
         }
     }
 
+    /**
+     * insert the value at the head of linked list.
+     * @param node_val
+     */
     public void insertFirst(int node_val){
         Node node = new Node(node_val);
         node.next = head;
         head = node;
     }
 
+    /**
+     * traverse the linked list and printout all the elements in it.
+     */
     public void traverse(){
         Node current = head;
-        while(current.next != null){
+        while(current != null) {
             System.out.println(current.value);
             current = current.next;
         }
-        System.out.println(current.value);
+    }
 
+    public void insertAtPosition(int position, int data){
+        Node node = new Node(data);
+        Node current = head;
 
+        //position = 0, insert the new node as head
+        if(position == 0){
+            node.next = head;
+            head = node;
+        }
+        for (int i = 0; i < position-1; i++) {
+            current = current.next;
+        }
+
+        node.next = current.next;
+        current.next = node;
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+            next = null;
+        }
     }
 
     public static void main(String[] args) {
@@ -38,7 +76,13 @@ public class LinkedList {
         list.insert(10);
         list.insert(20);
         list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.insert(60);
+
+        list.insertAtPosition(0, 70);
 
         list.traverse();
     }
+
 }
--- LinkedList/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    int value;
-    Node next;
-
-    public Node(int value) {
-        this.value = value;
-        next = null;
-    }
-}
