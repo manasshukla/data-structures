306375cdfa21d0d224d6a2dcbd5a1a3f435f9deb
Manas Shukla <mshukla@apple.com>
2019-12-07 19:32:37 +0530

    Adding the code for SimpleTextEditor

--- stack/SimpleTextEditor.java
+++ stack/SimpleTextEditor.java
@@ -1,54 +1,60 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class SimpleTextEditor {
 
     static String s = "";
     static Stack<String> stack = new Stack<>();
+    static List<Character> result = new ArrayList<>();
 
-    public static void append(String str){
+    public static void  append(String str){
         s = s+str;
         stack.push(s);
     }
 
     public static void delete(int k){
-        if(!stack.isEmpty()){
-            String currentString = stack.peek();
-            stack.push(currentString.substring(0,currentString.length()-k));
-        }
+            s = s.substring(0,s.length()-k);
+            stack.push(s);
     }
 
-    public static void print(int k){
-        if(!stack.isEmpty()){
-            System.out.println(stack.peek().charAt(k-1));
-        }
+    public static void  print(int k){
+           result.add(stack.peek().charAt(k-1));
     }
 
     public static void undo(){
-        if(!stack.isEmpty()){
-            stack.pop();
-        }
+           stack.pop();
+           s = stack.peek();
     }
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         stack.push(s);
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
         while (n > 0) {
-            int choice = sc.nextInt();
+            String inp = br.readLine();
+            String inpArr[] =  inp.split(" ");
+            int choice = Integer.parseInt(inpArr[0]);
+
             if(choice == 1) {
-                String val = sc.nextLine().trim();
+                String val = inpArr[1];
                 //append
                 append(val);
             } else if(choice == 2) {
                 //delete
-                int k = sc.nextInt();
+                int k = Integer.parseInt(inpArr[1]);
                 delete(k);
             } else if(choice == 3) {
                 //print
-                int k = sc.nextInt();
+                int k = Integer.parseInt(inpArr[1]);
                 print(k);
             }else if(choice == 4) {
                 //undo
@@ -59,6 +65,8 @@ public class SimpleTextEditor {
 
             n--;
         }
-        sc.close();
+        br.close();
+
+        result.stream().forEach(i -> System.out.println(i));
     }
 }
